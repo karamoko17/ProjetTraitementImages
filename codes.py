16bb8a05e984a77bcd73d2c277f3387968c593c4
karamoko17 <awa.karamoko19@inphb.ci>
2023-12-13 12:19:54 +0100

    ajout des filtres barbe, chien, lunettes

--- codes.py
+++ codes.py
@@ -6,6 +6,7 @@ import numpy as np
 import threading
 from matplotlib import pyplot as plt
 
+
 class WebcamApp:
     def __init__(self, window, window_title):
         self.window = window
@@ -28,6 +29,9 @@ class WebcamApp:
         self.overlay_snowflakes_flag = False
         self.change_background_flag = False
         self.detect_mouth_flag = False
+        self.detect_eye_flag = False
+        self.detect_nose_flag = False
+        self.detect_barbe_flag = False
         
         # Lancement du processus de mise à jour de la vidéo
         self.update()
@@ -48,13 +52,13 @@ class WebcamApp:
         # Bouton pour incrustrer des flocons de neige
         tk.Button(menu_frame, text="Flocons de neige", command=self.overlay_snowflakes).grid(row=0, column=2, padx=10)
 
-        tk.Button(menu_frame, text="Barbe", command=self.detect_mouth).grid(row=0, column=3, padx=10)
+        tk.Button(menu_frame, text="Barbe", command=self.detect_barbe).grid(row=0, column=3, padx=10)
 
         # Bouton pour lancer tous les traitements en même temps
-        tk.Button(menu_frame, text="Chien", command=self.apply_all).grid(row=0, column=4, padx=10)
+        tk.Button(menu_frame, text="Chien", command=self.detect_nose).grid(row=0, column=4, padx=10)
 
         # Bouton pour lancer tous les traitements en même temps
-        tk.Button(menu_frame, text="lunette", command=self.apply_all).grid(row=0, column=5, padx=10)
+        tk.Button(menu_frame, text="lunette", command=self.detect_eye).grid(row=0, column=5, padx=10)
 
         # Bouton pour lancer tous les traitements en même temps
         tk.Button(menu_frame, text="Appliquer Tout", command=self.apply_all).grid(row=0, column=6, padx=10)
@@ -70,12 +74,23 @@ class WebcamApp:
 
     def detect_mouth(self):
         self.detect_mouth_flag = not self.detect_mouth_flag
+    
+    def detect_eye(self):
+        self.detect_eye_flag = not self.detect_eye_flag
 
+    def detect_nose(self):
+        self.detect_nose_flag = not self.detect_nose_flag
+        
+    def detect_barbe(self):
+        self.detect_barbe_flag = not self.detect_barbe_flag
+    
     def apply_all(self):
         self.apply_sepia_flag = True
         self.overlay_snowflakes_flag = True
         self.change_background_flag = True
-        # self.detect_mouth_flag = True
+        self.detect_eye_flag = True
+        self.detect_nose_flag = True
+        self.detect_barbe_flag= True
 
     def update(self):
         # Capture la trame vidéo
@@ -93,6 +108,15 @@ class WebcamApp:
 
         if self.detect_mouth_flag:
             frame = self.detect_mouth_function(frame)
+        
+        if self.detect_eye_flag:
+            frame = self.detect_eye_function(frame)
+            
+        if self.detect_nose_flag:
+            frame = self.detect_nose_function(frame)
+            
+        if self.detect_barbe_flag:
+            frame = self.detect_barbe_function(frame)
 
         # Met à jour le canevas avec la nouvelle image
         self.photo = ImageTk.PhotoImage(image=Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)))
@@ -169,7 +193,7 @@ class WebcamApp:
 
         return result
     
-    def detect_mouth_function(self, image):
+    '''def detect_mouth_function(self, image):
         # Conversion en niveaux de gris
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -194,8 +218,151 @@ class WebcamApp:
                 image[y:y+h, x:x+w][alpha_indices] * (1.0 - resized_mouth_image[:, :, 3][alpha_indices] / 255.0)
             )
 
+        return image'''
+    
+    
+    def detect_eye_function(self, image):
+        # Conversion en niveaux de gris
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        
+        # Utilisez un classificateur Haar pour détecter la bouche
+        eye_cascade = cv2.CascadeClassifier('D:/M1 Lyon 2/Traitement d\'image/ProjetTraitementImages/haarcascades/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
+       
+        # Détecter les yeux dans l'image
+        eyes = eye_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+        # Chargement de l'image par défaut pour les lunettes
+        default_glasses_image = cv2.imread('D:/M1 Lyon 2/Traitement d\'image/ProjetTraitementImages/images/lunette.png', cv2.IMREAD_UNCHANGED)
+        # Load the cascade
+        face_cascade = cv2.CascadeClassifier('D:/M1 Lyon 2/Traitement d\'image/ProjetTraitementImages/haarcascades/haarcascades/haarcascade_frontalface_alt.xml')
+        # Détection des visages
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+        
+        # Dessiner l'image par défaut des lunettes dans les zones détectées
+        for (x, y, w, h) in faces:
+            # Région d'intérêt (ROI) pour les yeux dans le visage détecté
+            roi_gray = gray[y:y + h, x:x + w]
+            roi_color = image[y:y + h, x:x + w]
+
+            # Détection des yeux dans la ROI
+            eyes_in_roi = eye_cascade.detectMultiScale(roi_gray)
+
+            # Stocker les coordonnées des yeux
+            eye_coords = []
+
+            # Pour chaque paire d'yeux détectée
+            for (ex, ey, ew, eh) in eyes_in_roi:
+                eye_coords.append((ex, ey, ew, eh))
+
+            # Si deux yeux sont détectés
+            if len(eye_coords) == 2:
+                # Calculer la position moyenne des deux yeux
+                avg_x = int((eye_coords[0][0] + eye_coords[1][0]) / 2)
+                avg_y = int((eye_coords[0][1] + eye_coords[1][1]) / 2)
+                avg_w = int((eye_coords[0][2] + eye_coords[1][2]) / 2)
+                avg_h = int((eye_coords[0][3] + eye_coords[1][3]) / 2)
+
+                # Redimensionner les lunettes à la taille des deux yeux
+                resized_sunglasses = cv2.resize(default_glasses_image, (avg_w * 3, avg_h * 2))
+
+                # Superposer les lunettes sur l'image
+                x_offset, y_offset = avg_x - avg_w, avg_y - avg_h // 2  # Ajuster la position des lunettes
+                y1, y2 = y_offset, y_offset + resized_sunglasses.shape[0]
+                x1, x2 = x_offset, x_offset + resized_sunglasses.shape[1]
+            
+
+                # Région de l'image pour les lunettes
+                alpha_s = resized_sunglasses[:, :, 3] / 255.0
+                alpha_l = 1.0 - alpha_s
+                for c in range(0, 3):
+                    roi_color[y1:y2, x1:x2, c] = (alpha_s * resized_sunglasses[:, :, c] + alpha_l * roi_color[y1:y2, x1:x2, c])
+                
         return image
     
+    
+    def detect_nose_function(self, image):
+        # Conversion en niveaux de gris
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Utilisez un classificateur Haar pour détecter le nez
+        nose_cascade = cv2.CascadeClassifier('D:/M1 Lyon 2/Traitement d\'image/ProjetTraitementImages/haarcascades/haarcascades/haarcascade_mcs_nose.xml')
+       
+       # Charger l'image par défaut pour le nez de chien
+        default_nose_image = cv2.imread('D:/M1 Lyon 2/Traitement d\'image/ProjetTraitementImages/images/nezchien.png', cv2.IMREAD_UNCHANGED)
+
+        # Load the cascade
+        face_cascade = cv2.CascadeClassifier('D:/M1 Lyon 2/Traitement d\'image/ProjetTraitementImages/haarcascades/haarcascades/haarcascade_frontalface_alt.xml')
+        # Détection des visages
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+    
+        
+        # Ajuster cette valeur pour contrôler la position verticale du nez
+        offset_y_percentage = 0.15
+
+        for (x, y, w, h) in faces:
+            # Ajuster la taille de l'image du nez pour mieux s'adapter à l'ensemble du visage
+            resized_nose = cv2.resize(default_nose_image, (w, h))
+
+            # Région d'intérêt (ROI) pour le nez dans le visage détecté
+            roi_color = image[y:y + h, x:x + w]
+
+            # Assurer que les coordonnées sont valides
+            if 0 <= y < image.shape[0] and 0 <= y + h < image.shape[0] and 0 <= x < image.shape[1] and 0 <= x + w < image.shape[1]:
+                # Région de l'image pour le nez
+                alpha_n = resized_nose[:, :, 3] / 255.0
+                alpha_l = 1.0 - alpha_n
+
+                # Déplacer l'image du nez vers le haut en ajustant les coordonnées y
+                offset_y = int(h * offset_y_percentage)
+                y_offset = y - offset_y if y - offset_y >= 0 else 0
+
+                for c in range(0, 3):
+                    # Appliquer l'image du nez sur l'ensemble du visage
+                    image[y_offset:y_offset + h, x:x + w, c] = (alpha_n * resized_nose[:, :, c] + alpha_l * image[y_offset:y_offset + h, x:x + w, c])
+        return image   
+    
+    def detect_barbe_function(self, image):
+        # Conversion en niveaux de gris
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+       # Charger l'image par défaut pour le nez de chien
+        default_nose_image = cv2.imread('D:/M1 Lyon 2/Traitement d\'image/ProjetTraitementImages/images/barbe.png', cv2.IMREAD_UNCHANGED)
+
+        # Load the cascade
+        face_cascade = cv2.CascadeClassifier('D:/M1 Lyon 2/Traitement d\'image/ProjetTraitementImages/haarcascades/haarcascades/haarcascade_frontalface_alt.xml')
+        # Détection des visages
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30))
+    
+        
+        # Ajuster cette valeur pour contrôler la position verticale du nez vers le bas
+        offset_y_percentage = 0.3 #0.45
+
+        for (x, y, w, h) in faces:
+            # Ajuster la taille de l'image du nez pour mieux s'adapter à l'ensemble du visage
+            resized_nose = cv2.resize(default_nose_image, (w, h))
+
+            # Région d'intérêt (ROI) pour le nez dans le visage détecté
+            roi_color = image[y:y + h, x:x + w]
+
+            # Assurer que les coordonnées sont valides
+            if 0 <= y < image.shape[0] and 0 <= y + h < image.shape[0] and 0 <= x < image.shape[1] and 0 <= x + w < image.shape[1]:
+                # Région de l'image pour le nez
+                alpha_n = resized_nose[:, :, 3] / 255.0
+                alpha_l = 1.0 - alpha_n
+
+                # Déplacer l'image du nez vers le bas en ajustant les coordonnées y
+                offset_y = int(h * offset_y_percentage)
+                y_offset = y + offset_y if y + h + offset_y <= image.shape[0] else y
+
+                for c in range(0, 3):
+                    # Appliquer l'image du nez sur l'ensemble du visage
+                    image[y_offset:y_offset + h, x:x + w, c] = (alpha_n * resized_nose[:, :, c] + alpha_l * image[y_offset:y_offset + h, x:x + w, c])
+        
+        return image   
+    
+    
+    
+
     def on_close(self):
         # Libération de la capture vidéo lors de la fermeture de l'application
         if self.vid.isOpened():
